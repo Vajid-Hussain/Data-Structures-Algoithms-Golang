d92b8b49862f476d3f2b07a81cad57641e82a053
Vajid-Hussain <vajidhussain17@gmail.com>
2024-01-30 16:18:55 +0530

    2848

---
+++ HashTable/2848.go
@@ -0,0 +1,9 @@
+// func numberOfPoints(nums [][]int) int {
+//     var count =make(map[int]struct{})
+//     for _,val:=range nums{
+//         for i:=val[0]; i<=val[1]; i++{
+//             count[i]=struct{}{}
+//         }
+//     }
+//     return len(count)
+// }
