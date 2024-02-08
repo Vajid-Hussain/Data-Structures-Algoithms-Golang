0fa351cf8da3ebe4d1a04641aaf50fe02498a7dc
Vajid-Hussain <vajidhussain17@gmail.com>
2024-02-08 11:18:01 +0530

    206

---
+++ linkedList/singleLinkedList/206.go
@@ -0,0 +1,31 @@
+// /**
+//  * Definition for singly-linked list.
+//  * type ListNode struct {
+//  *     Val int
+//  *     Next *ListNode
+//  * }
+//  */
+//  var newHead *ListNode
+
+//  func reverseList(head *ListNode) *ListNode {
+// 	 if head==nil{
+// 		 return head
+// 	 }
+// 	 recursions(head)
+// 	 head.Next = nil
+// 	 return newHead
+//  }
+ 
+//  func recursions(current *ListNode) {
+// 	 if current.Next == nil {
+// 		 newHead = current
+// 		 return
+// 	 }
+// 	 prev := current
+// 	 Next := current.Next
+ 
+// 	 recursions(Next)
+// 	 Next.Next = prev
+//  }
+ 
+//second solution
