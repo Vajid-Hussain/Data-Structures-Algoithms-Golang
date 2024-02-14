f744dc109dbf34987a4a0ed07e624d77b2a88892
Vajid-Hussain <vajidhussain17@gmail.com>
2024-02-14 13:13:53 +0530

    1295

---
+++ array/1295.go
@@ -0,0 +1,10 @@
+// func findNumbers(nums []int) int {
+//     var totalSum int
+//     for _,val:=range nums{
+//         valStr:=strconv.Itoa(val)
+//         if len(valStr)%2==0{
+//             totalSum++
+//         }
+//     }
+//     return totalSum
+// }
