1f8980ecd1ed51be3370672227fc5a136409ede2
Vajid-Hussain <vajidhussain17@gmail.com>
2024-02-23 09:07:12 +0530

    2942

---
+++ array/2942.go
@@ -0,0 +1,13 @@
+// func findWordsContaining(words []string, x byte) []int {
+//     var result = []int{}
+// 	for pos,val:=range words{
+//         for i:=0; i<len(val); i++{
+//             if val[i]==x{
+//                 result=append(result, pos)
+//                 break
+//             }
+//         }
+//     }
+//     return result
+// }
+
