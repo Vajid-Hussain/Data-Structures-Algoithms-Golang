a07a28bc7040fa8d01c852820e905b0d8afe0dea
Vajid-Hussain <vajidhussain17@gmail.com>
2024-03-01 09:00:17 +0530

    326

---
+++ Recursion/326.go
@@ -0,0 +1,13 @@
+// func isPowerOfThree(n int) bool {
+//     return power(1, n)
+// }
+
+// func power(result, n int) bool{
+//     if result == n{
+//         return true
+//     }
+//     if result > n{
+//         return false
+//     }
+//     return power(result*3, n)
+// }
