71cb4e97fc3a5c0ad6d617f3de25f014e3973d13
Vajid-Hussain <vajidhussain17@gmail.com>
2024-03-11 08:32:20 +0530

    1641

---
+++ DS_patterns/TwoPointers/1641.go
@@ -0,0 +1,11 @@
+// func countVowelStrings(n int) int {
+//     var vowels=[]int{1,2,3,4,5}
+
+//     for i:=1; i<n; i++{
+//         for j:=1; j<5; j++{
+//             // fmt.Println(j)
+//             vowels[j]+=vowels[j-1]
+//         }
+//     }
+//     return vowels[4]
+// }
