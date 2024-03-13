6dc31a8e632697d2dce4aed0850563056b1ca448
Vajid-Hussain <vajidhussain17@gmail.com>
2024-03-13 08:27:52 +0530

    1347

--- sample.go
+++ sample.go
@@ -3,13 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("--", fibanocci(8))
+	var arr = []int{4, 65, 0, 7, 9, 3}
+	fmt.Println("==", mergeSort(arr))
 }
 
-func fibanocci(n int) int {
-	if n <= 1 {
-		return n
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	first := mergeSort(arr[len(arr)/2:])
+	second := mergeSort(arr[:len(arr)/2])
+	return merge(first, second)
+}
+
+func merge(arr1, arr2 []int) []int {
+	var final []int
+	var i, j int
+	for i < len(arr1) && j < len(arr2) {
+		if arr1[i] < arr2[j] {
+			final = append(final, arr1[i])
+			i++
+		} else {
+			final = append(final, arr2[j])
+			j++
+		}
 	}
 
-	return sum
+	for ; i < len(arr1); i++ {
+		final = append(final, arr1[i])
+	}
+	for ; j < len(arr2); j++ {
+		final = append(final, arr2[j])
+	}
+	return final
 }
