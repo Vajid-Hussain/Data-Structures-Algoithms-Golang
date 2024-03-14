19d9a0d3236e43da1e98a37d5c2f970ca684cba8
Vajid-Hussain <vajidhussain17@gmail.com>
2024-03-14 09:39:26 +0530

    287

--- DS_patterns/TwoPointers/2562.go
+++ DS_patterns/TwoPointers/2562.go
@@ -1,18 +1,18 @@
-func findTheArrayConcVal(nums []int) int64 {
-	var i, j, result int
-	j = len(nums) - 1
-	for i < j {
-		first := strconv.Itoa(nums[i])
-		second := strconv.Itoa(nums[j])
-		mixed:=first+second
-        secondInt, _ := strconv.Atoi(mixed)
-		result += secondInt 
+// func findTheArrayConcVal(nums []int) int64 {
+// 	var i, j, result int
+// 	j = len(nums) - 1
+// 	for i < j {
+// 		first := strconv.Itoa(nums[i])
+// 		second := strconv.Itoa(nums[j])
+// 		mixed:=first+second
+//         secondInt, _ := strconv.Atoi(mixed)
+// 		result += secondInt 
 
-		i++
-		j--
-	}
-	if len(nums)%2 != 0 {
-		result += nums[len(nums)/2]
-	}
-	return int64(result)
-}
+// 		i++
+// 		j--
+// 	}
+// 	if len(nums)%2 != 0 {
+// 		result += nums[len(nums)/2]
+// 	}
+// 	return int64(result)
+// }
---
+++ array/287.go
@@ -0,0 +1,10 @@
+// func findDuplicate(nums []int) int {
+//     slices.Sort(nums)
+
+//     for i:=0; i<len(nums)-1; i++{
+//         if nums[i]==nums[i+1]{
+//             return nums[i]
+//         }
+//     }
+//     return 0
+// }
