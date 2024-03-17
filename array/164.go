244f8e4b632ae5be6ffb41283395089d708b6e60
Vajid-Hussain <vajidhussain17@gmail.com>
2024-03-17 09:02:09 +0530

    164

---
+++ array/164.go
@@ -0,0 +1,10 @@
+// func maximumGap(nums []int) int {
+//     slices.Sort(nums)
+//     var maxDiff int
+//     for i:=0; i<len(nums)-1; i++{
+//         if nums[i+1]-nums[i] > maxDiff{
+//             maxDiff= nums[i+1]-nums[i]
+//         }
+//     }
+//     return maxDiff
+// }
