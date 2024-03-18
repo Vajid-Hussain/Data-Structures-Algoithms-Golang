7581839830a5b11ffa5c668098fc6bab5a696c8a
Vajid-Hussain <vajidhussain17@gmail.com>
2024-03-18 08:45:49 +0530

    22

--- Recursion/22.go
+++ Recursion/22.go
@@ -20,3 +20,30 @@
 //         rec(result, n, left, right+1, current)
 //     }
 // }
+
+//second way
+
+// func generateParenthesis(n int) []string {
+//     ans:=make([]string,0)
+//     current:= make([]byte, n*2)
+//     rec22(&ans, n, 0, 0, current)
+//     return ans
+// }
+
+// func rec22(ans *[]string, n int, left int, right int, current []byte){
+//     // fmt.Printf("%T",n)
+//     if left+right== n*2{
+//         *ans= append(*ans, string(current))
+//         return
+//     }
+
+//     if left < n{
+//         current[left+right]='('
+//         rec22(ans, n,left+1, right, current)
+//     }
+
+//     if right < left{
+//         current[left+right] = ')'
+//         rec22(ans, n,left, right+1, current)
+//     }temipromter
+// }
