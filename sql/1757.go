8263624d6c8339913aaf53f5f98c98306c86a6f4
Vajid-Hussain <vajidhussain17@gmail.com>
2024-05-15 09:32:11 +0530

    sql started

---
+++ sql/1757.go
@@ -0,0 +1,5 @@
+// -- Write your PostgreSQL query statement below
+// SELECT product_id FROM products WHERE low_fats='Y' AND recyclable='Y';
+
+// # Write your MySQL query statement below
+// SELECT product_id FROM products WHERE low_fats= 'Y' AND recyclable = 'Y';
